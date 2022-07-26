960918286d1db442d178133ded303a74140e05bb
Apostolis-Del <del13killer@gmail.com>
2022-07-26 11:43:12 +0300

    final

---
+++ github-finder/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <h2>
+            A React App that searches Github profiles. Ναι μόνο αυτό κάνει.
+        </h2>
+    </div>
+  )
+}
+
+export default About
